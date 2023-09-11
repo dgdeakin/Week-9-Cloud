6a8b22f977671a59d08ca2f36d3003656a41fdb1
Dev <d.gurung@deakin.edu.au>
2023-09-11 21:39:33 +1000

    first commit

---
+++ week9cloud/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <div class="myDiv">This is home page!</div>
+
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style scoped>
+.myDiv {
+  border: 5px outset red;
+  background-color: lightblue;
+  text-align: center;
+  height: 400px;
+}
+</style>
---
+++ week9cloud/src/views/ProjectsView.vue
@@ -0,0 +1,26 @@
+<script setup>
+// Vue js code here
+
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <div>This is projects Page!</div>
+    <h1>{{ $route.params.id }}</h1>
+    <RouterView />
+  </div>
+
+  <!-- html codes -->
+</template>
+
+<style scoped>
+/* Any css  */
+
+.about {
+  border: 5px outset rebeccapurple;
+  background-color: aquamarine;
+  text-align: center;
+  height: 300px;
+}
+</style>
